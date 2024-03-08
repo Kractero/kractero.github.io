84468a68bf3ebc82a6d5f6293d18e587c4ef02fd
Kractero <kractero@sideroca.com>
2024-03-08 13:14:45 -0700

    Tech

---
+++ src/components/Techs.ts
@@ -0,0 +1,37 @@
+export const techs = [
+	{
+        "name": "SvelteKit",
+        "url": "https://kit.svelte.dev/",
+        "img": "Svelte.png"
+    },
+    {
+        "name": "Next.js",
+        "url": "https://nextjs.org/",
+        "img": "Next.png"
+    },
+    {
+        "name": "JavaScript",
+        "url": "vanilla-js.com",
+        "img": "JavaScript.png"
+    },
+    {
+        "name": "TypeScript",
+        "url": "https://www.typescriptlang.org/",
+        "img": "TypeScript.png"
+    },
+    {
+        "name": "Python",
+        "url": "https://www.python.org/",
+        "img": "Python.png"
+    },
+    {
+        "name": "Express",
+        "url": "https://expressjs.com/",
+        "img": "Express.png"
+    },
+    {
+        "name": "Docker",
+        "url": "https://www.docker.com/",
+        "img": "Docker.png"
+    },
+];
