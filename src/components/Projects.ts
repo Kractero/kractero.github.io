1137f7648855aaad1367aac2f87651f293b967ec
Kractero <kractero@sideroca.com>
2024-03-08 08:15:15 -0700

    basic layout and projects

---
+++ src/components/Projects.ts
@@ -0,0 +1,50 @@
+export const projects = [
+	{
+		url: "https://github.com/Kractero/hare",
+		name: "Hare",
+		desc: "Humble Automated Runner for Executing NationStates Scripts",
+		techs: ["SvelteKit", "TypeScript", "Tailwind"],
+	},
+	{
+		url: "https://github.com/Kractero/ledger",
+		name: "Ledger",
+		desc: "View detailed data on the cards top 100 and the status of legendaries.",
+		techs: ["JavaScript"],
+	},
+	{
+		url: "https://github.com/Kractero/cardtainers",
+		name: "Cardtainers",
+		desc: "A drop-in replacement for Containerise for trading cards.",
+		techs: ["JavaScript"],
+	},
+	{
+		url: "https://github.com/Kractero/kee",
+		name: "Card Queries (Kee ver.)",
+		desc: "Queries for cards related tasks.",
+		techs: ["SvelteKit", "TypeScript", "Tailwind", "Express"],
+	},
+	{
+		url: "https://github.com/Kractero/cardqueries",
+		name: "Card Queries (Sideroca ver.)",
+		desc: "Older queries for cards related tasks.",
+		techs: ["Next.js", "TypeScript", "Tailwind", "FastAPI", "PostgreSQL"],
+	},
+	{
+		url: "https://github.com/Kractero/bazaar",
+		name: "Bazaar",
+		desc: "NationStates trade records since April 1 2018.",
+		techs: ["Express", "JavaScript", "Tailwind"],
+	},
+  {
+		url: "https://github.com/Kractero/megu",
+		name: "Megu",
+		desc: "View historical market value of any card with incorrect values.",
+		techs: ["Next.js", "TypeScript"],
+	},
+  {
+		url: "https://github.com/Kractero/2023",
+		name: "2023 Yearly Report",
+		desc: "2023 Yearly Numbers in cards ",
+		techs: ["Python", "SQL"],
+	},
+];
