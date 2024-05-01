751d8f183f7dc827a3d6fe1d78de2f2575c95a9c
Kractero <kractero@sideroca.com>
2024-05-01 16:27:46 -0700

    change some link urls to deployment

--- src/components/Projects.ts
+++ src/components/Projects.ts
@@ -1,12 +1,12 @@
 export const projects = [
 	{
-		url: "https://github.com/Kractero/hare",
+		url: "https://hare.kractero.com",
 		name: "Hare",
 		desc: "Humble Automated Runner for Executing NationStates Scripts",
 		techs: ["SvelteKit", "TypeScript", "Tailwind"],
 	},
 	{
-		url: "https://github.com/Kractero/ledger",
+		url: "https://ledger.kractero.com",
 		name: "Ledger",
 		desc: "View detailed data on the cards top 100 and the status of legendaries.",
 		techs: ["JavaScript"],
@@ -18,31 +18,37 @@ export const projects = [
 		techs: ["JavaScript"],
 	},
 	{
-		url: "https://github.com/Kractero/kee",
+		url: "https://queries.kractero.com",
 		name: "Card Queries (Kee ver.)",
 		desc: "Queries for cards related tasks.",
 		techs: ["SvelteKit", "TypeScript", "Tailwind", "Express"],
 	},
 	{
 		url: "https://github.com/Kractero/cardqueries",
-		name: "Card Queries (Sideroca ver.)",
+		name: "Card Queries (Sideroca, Deprecated)",
 		desc: "Older queries for cards related tasks.",
 		techs: ["Next.js", "TypeScript", "Tailwind", "FastAPI", "PostgreSQL"],
 	},
 	{
-		url: "https://github.com/Kractero/bazaar",
+		url: "https://kractero.com/post-crash",
+		name: "Post Crash",
+		desc: "See damage done by the cards database loss.",
+		techs: ["JavaScript", "Python"],
+	},
+	{
+		url: "https://bazaar.kractero.com",
 		name: "Bazaar",
 		desc: "NationStates trade records since April 1 2018.",
 		techs: ["Express", "JavaScript", "Tailwind"],
 	},
-  {
+  	{
 		url: "https://github.com/Kractero/megu",
 		name: "Megu",
 		desc: "View historical market value of any card with incorrect values.",
 		techs: ["Next.js", "TypeScript"],
 	},
-  {
-		url: "https://github.com/Kractero/2023",
+  	{
+		url: "https://www.nationstates.net/page=dispatch/id=1982277",
 		name: "2023 Yearly Report",
 		desc: "2023 Yearly Numbers in cards ",
 		techs: ["Python", "SQL"],
