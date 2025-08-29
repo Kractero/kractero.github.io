3b8984cb8eb20d195bb9dadf4c0cfbb87ae36dee
Kractero <nationstates@kractero.com>
2025-08-29 16:13:48 -0700

    update packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://kractero.github.io',
-  integrations: [tailwind()]
-});
+  vite: { plugins: [tailwindcss()] },
+})
